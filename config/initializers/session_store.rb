ec6a8a13b0e5b326baf5193ce56b69b1f7fbc542
bodhi <bodhi@5263.org>
2015-05-20 02:31:16 +1000

    Disable httpOnly for session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SecurityApp::Application.config.session_store :cookie_store, key: '_security_app_session'
+SecurityApp::Application.config.session_store :cookie_store, key: '_security_app_session', :httponly => false
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
